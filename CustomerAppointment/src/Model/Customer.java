6076f887e952cbce78e20959cf368919ebd0e856
Travis <travisseal@gmail.com>
2017-03-21 22:01:35 -0700

    data model for customer

--- CustomerAppointment/src/Model/Customer.java
+++ CustomerAppointment/src/Model/Customer.java
@@ -3,6 +3,8 @@
  */
 package Model;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 
@@ -15,4 +17,27 @@ public class Customer extends Person{
     private StringProperty address;
     private ObservableValue<Integer> addressId;
     
+    public ObservableValue<String> getAddress()
+    {
+        return this.address;
+    }
+    
+    public void setAddress(String addr)
+    {
+        this.address = new SimpleStringProperty(addr);
+    }
+    
+    public void setAddressId(int id)
+    {
+        this.addressId = new SimpleIntegerProperty(id).asObject();
+    }
+    public void setActive (Boolean act)
+    {
+        isActive = act;
+    }
+    
+    public void setCreateDate(StringProperty cDate)
+    {
+        createDate = cDate;
+    }
 }
